738120171db622f864b55b2bdf29a9f549a13a71
sbabaiev <serhii@nexar.app>
2025-02-09 15:11:02 +0200

    Add NavLink component

--- components/news-list.js
+++ components/news-list.js
@@ -1,6 +1,8 @@
 import Link from "next/link";
 
 export default function NewsList({ news }) {
+  console.log("news list");
+
   return (
     <ul className="news-list">
       {news.map((newsItem, index) => (
